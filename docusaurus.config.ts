6a76efb9263cb912d847752b5c530e853cde6de4
Henry <211283243+luonghenry@users.noreply.github.com>
2026-02-26 08:22:06 +0900

    Fix formatting issue in docusaurus.config.ts

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -61,7 +61,7 @@ const config: Config = {
           blogSidebarCount: 'ALL',
           blogSidebarTitle: 'All posts',
           // TOC level cho blog
-          blogPostComponent: '@theme/BlogPostPage'
+          blogPostComponent: '@theme/BlogPostPage',
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
